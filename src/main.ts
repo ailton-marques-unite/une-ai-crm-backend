bedc444e7bf36e07d7b6c7658a1a7beb152e0b83
ailton-marques-unite <ailton.marques@une.cx>
2025-12-11 16:00:11 -0300

    adjusted cors parameters

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuração de CORS
+  app.enableCors({
+    origin: 'http://localhost:43779',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
 const config = new DocumentBuilder()
     .setTitle('CRM Backend Service API - NestJS')
     .setDescription('API documentation for the CRM Backend Service built with NestJS')
